513f5f667aa709f195c7b89ef3229e87c150308e
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-07-02 21:09:06 +0100

    Created a hashmap to store alphabets and corresponding morse code

--- src/main/java/com/example/morsecodeconverter/Controller.java
+++ src/main/java/com/example/morsecodeconverter/Controller.java
@@ -5,8 +5,12 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import java.util.HashMap;
 
 public class Controller {
+
+    private String englishText;
+    private HashMap<Character, String> map = new HashMap<>();
 
     @FXML
     private TextArea englishInput;
@@ -17,8 +21,38 @@ public class Controller {
     @FXML
     private Button playSoundButton;
 
+    public Controller() {
+        map.put('A', ".-");
+        map.put('B', "-…");
+        map.put('C', "-.-.");
+        map.put('D' , "-..");
+        map.put('E', ".");
+        map.put('F', "..-.");
+        map.put('G', "–.");
+        map.put('H', "….");
+        map.put('I', "..");
+        map.put('J', ".—");
+        map.put('K', "-.-");
+        map.put('L', ".-..");
+        map.put('M', "–");
+        map.put('N', "-.");
+        map.put('O', "—");
+        map.put('P', ".–.");
+        map.put('Q', "–.-");
+        map.put('R', ".-.");
+        map.put('S', "…");
+        map.put('T', "-");
+        map.put('U', "..-");
+        map.put('V', "…-");
+        map.put('W', ".–");
+        map.put('X', "-..-");
+        map.put('Y', "-.–");
+        map.put('Z', "–..");
+    }
+
     @FXML
     void displayMorseCodeWhenKeyReleased(KeyEvent event) {
+        englishText = englishInput.getText();
 
     }
 
@@ -28,3 +62,4 @@ public class Controller {
     }
 
 }
+
