b2b3034e0e97bce9c86b44904669cc03c702940f
mathewpeedikayil <mathewphilip.peedikayil@gmail.com>
2023-07-03 14:10:42 +0100

    Updated module info with media dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.morsecodeconverter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
 
     opens com.example.morsecodeconverter to javafx.fxml;
